020f14558ad1eebdea827e11dfc39684868046ba
Florian Thiel <flth@flth.de>
2023-11-22 21:14:17 +0100

    initial

---
+++ strings.lua
@@ -0,0 +1,143 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Container wagon Sgns(s) 60",
+		["mod_desc"] = "The Sgns 60 is a container wagon for intermodal transport with a length of 19.74 m. This wagon has a loading length of 60 ft and can therefore be loaded with containers according to the ISO standard as well as with the European 30 ft container.",
+
+		["option_fake_wagen_desc"] = "Should the fake versions of the Sgns(s) 60 be available in the game?",
+		["Fake_sgns60_wagen"] = "Activate fake versions?",
+
+        ["sgns_menu"] = "Sgns(s) 60 container wagon",
+		["sgns_aae_name"] = "Sgns 60 AAE (1991)",
+		["sgns_db_name"] = "Sgns 60 DB (1987)",
+		["sgns_db2_name"] = "Sgns 60 DBAG (1996)",
+		["sgns_db3_name"] = "Sgns 60 DBAG (2005)",
+		["sgns_ermewa_name"] = "Sgnss 60 Ermewa (1999)",
+		["sgns_ermewa2_name"] = "Sgnss 60 Ermewa (2016)",
+		["sgns_err_name"] = "Sgnss 60 ERR (2006)",
+		["sgns_gatx_name"] = "Sgns 60 GATX (2019)",
+		["sgns_gts_name"] = "Sgnss 60 GTS (2011)",
+		["sgns_hupac_name"] = "Sgnss 60 Hupac (1981)",
+		["sgns_hupac2_name"] = "Sgnss 60 Hupac (2000)",
+		["sgns_hupac3_name"] = "Sgnss 60 Hupac (2023)",
+		["sgns_kombiverkehr_name"] = "Sgns 60 Kombiwaggon (1991)",
+		["sgns_mfd_name"] = "Sgns 60 MFD (2020)",
+		["sgns_rca_name"] = "Sgnss 60 RCA (2008)",
+		["sgns_rca2_name"] = "Sgnss 60 RCA (1990)",
+		["sgns_sbb_name"] = "Sgns 60 SBB (1995)",
+		["sgns_stb_name"] = "Sgnss 60 StB (2005)",
+		["sgns_vtg_name"] = "Sgnss 60 VTG (2000)",
+        ["sgns_desc"] = "The Sgns 60 is a container wagon for intermodal transport with a length of 19.74 m. This wagon has a loading length of 60 ft and can therefore be loaded with containers according to the ISO standard as well as with the European 30 ft container.",
+
+		["sgssy_name"] = "Sgss-y 703 Kombiwaggon (1991)",
+		["sgssy2_name"] = "Sgss 703 Kombiwaggon (1998)",
+		["sgssy2_desc"] = "Sgss(-y) 703 are container wagons for PIC (Parcel Intercity) high-speed traffic",
+
+		--- Ganzzug ---
+		["sgns_gts_name_gts"] = "Sgnss 60 GTS (2011) GTS",
+		["sgssy_name_pic"] = "Sgss-y 703 Kombiwaggon (1991) PIC",
+		["sgssy2_name_pic"] = "Sgss 703 Kombiwaggon (1998) PIC",
+
+		--- Fake ---
+        ["sgns_menu"] = "Sgns(s) 60 ontainer wagon Fake",
+		["sgns_aae_name_fake"] = "Sgns 60 AAE (1991) Fake",
+		["sgns_db_name_fake"] = "Sgns 60 DB (1987) Fake",
+		["sgns_db2_name_fake"] = "Sgns 60 DBAG (1996) Fake",
+		["sgns_db3_name_fake"] = "Sgns 60 DBAG (2005) Fake",
+		["sgns_ermewa_name_fake"] = "Sgnss 60 Ermewa (1999) Fake",
+		["sgns_ermewa2_name_fake"] = "Sgnss 60 Ermewa (2016) Fake",
+		["sgns_err_name_fake"] = "Sgnss 60 ERR (2006) Fake",
+		["sgns_gatx_name_fake"] = "Sgns 60 GATX (2019) Fake",
+		["sgns_gts_name_fake"] = "Sgnss 60 GTS (2011) Fake",
+		["sgns_hupac_name_fake"] = "Sgnss 60 Hupac (1981) Fake",
+		["sgns_hupac2_name_fake"] = "Sgnss 60 Hupac (2000) Fake",
+		["sgns_hupac3_name_fake"] = "Sgnss 60 Hupac (2023) Fake",
+		["sgns_kombiverkehr_name_fake"] = "Sgns 60 Kombiwaggon (1991) Fake",
+		["sgns_mfd_name_fake"] = "Sgns 60 MFD (2020) Fake",
+		["sgns_rca_name_fake"] = "Sgnss 60 RCA (2008) Fake",
+		["sgns_rca2_name_fake"] = "Sgnss 60 RCA (1990) Fake",
+		["sgns_sbb_name_fake"] = "Sgnss 60 SBB (1995) Fake",
+		["sgns_stb_name_fake"] = "Sgnss 60 StB (2005) Fake",
+		["sgns_vtg_name_fake"] = "Sgns 60 VTG (2000) Fake",
+
+		["sgssy_name_fake"] = "Sgss-y 703 Kombiwaggon (1991) Fake",
+		["sgssy2_name_fake"] = "Sgss 703 Kombiwaggon (1998) Fake",
+
+		--- Fake Ganzzug ---
+		["sgns_gts_name_gts_fake"] = "Sgnss 60 GTS (2011) GTS Fake",
+		["sgssy_name_pic_fake"] = "Sgss-y 703 Kombiwaggon PIC (1991) Fake",
+		["sgssy2_name_pic_fake"] = "Sgss 703 Kombiwaggon PIC (1998) Fake",
+
+	},
+	de = {
+
+		["mod_name"] = "Sgns(s) 60 Containerwagen",
+		["mod_desc"] = "Der Sgns 60 ist ein Containerwagen für den Intermodalen Verkehr mit einer Länge von 19,74 m. Dieser Wagen hat eine Ladelänge von 60 ft und kann dadurch mit Container nach der ISO Norm sowie mit den Europäischen 30 ft Container beladen werden.",
+
+		["option_fake_wagen_desc"] = "Sollen die Fake-Versionen des Sgns(s) 60 verfügbar sein im Spiel?",
+		["Fake_sgns60_wagen"] = "Fake-Versionen aktivieren?",
+
+        ["sgns_menu"] = "Sgns(s) 60 Containertragwagen",
+		["sgns_aae_name"] = "Sgns 60 AAE (1991)",
+		["sgns_db_name"] = "Sgns 60 DB (1987)",
+		["sgns_db2_name"] = "Sgns 60 DBAG (1996)",
+		["sgns_db3_name"] = "Sgns 60 DBAG (2005)",
+		["sgns_ermewa_name"] = "Sgnss 60 Ermewa (1999)",
+		["sgns_ermewa2_name"] = "Sgnss 60 Ermewa (2016)",
+		["sgns_err_name"] = "Sgnss 60 ERR (2006)",
+		["sgns_gatx_name"] = "Sgns 60 GATX (2019)",
+		["sgns_gts_name"] = "Sgnss 60 GTS (2011)",
+		["sgns_hupac_name"] = "Sgnss 60 Hupac (1981)",
+		["sgns_hupac2_name"] = "Sgnss 60 Hupac (2000)",
+		["sgns_hupac3_name"] = "Sgnss 60 Hupac (2023)",
+		["sgns_kombiverkehr_name"] = "Sgns 60 Kombiverkehr (1991)",
+		["sgns_mfd_name"] = "Sgns 60 MFD (2020)",
+		["sgns_rca_name"] = "Sgnss 60 RCA (2008)",
+		["sgns_rca2_name"] = "Sgnss 60 RCA (1990)",
+		["sgns_sbb_name"] = "Sgns 60 SBB (1995)",
+		["sgns_stb_name"] = "Sgnss 60 StB (2005)",
+		["sgns_vtg_name"] = "Sgnss 60 VTG (2000)",
+        ["sgns_desc"] = "Der Sgns 60 ist ein Containerwagen für den Intermodalen Verkehr mit einer Länge von 19,74 m. Dieser Wagen hat eine Ladelänge von 60 ft und kann dadurch mit Container nach der ISO Norm sowie mit den Europäischen 30 ft Container beladen werden.",
+
+		["sgssy_name"] = "Sgss-y 703 Kombiwaggon (1991)",
+		["sgssy2_name"] = "Sgss 703 Kombiwaggon (1998)",
+		["sgssy2_desc"] = "Sgss(-y) 703 sind Containertragwagen für den PIC(Parcel Intercity) Hochgeschwindigkeitsverkehr",
+
+		--- Ganzzug ---
+		["sgns_gts_name_gts"] = "Sgnss 60 GTS (2011)",
+		["sgssy_name_pic"] = "Sgss-y 703 Kombiwaggon PIC (1991)",
+		["sgssy2_name_pic"] = "Sgss 703 Kombiwaggon PIC (1998)",
+
+		--- Fake ---
+        ["sgns_menu_fake"] = "Sgns(s) 60 Containertragwagen Fake",
+		["sgns_aae_name_fake"] = "Sgns 60 AAE (1991) Fake",
+		["sgns_db_name_fake"] = "Sgns 60 DB (1987) Fake",
+		["sgns_db2_name_fake"] = "Sgns 60 DBAG (1996) Fake",
+		["sgns_db3_name_fake"] = "Sgns 60 DBAG (2005) Fake",
+		["sgns_ermewa_name_fake"] = "Sgnss 60 Ermewa (1999) Fake",
+		["sgns_ermewa2_name_fake"] = "Sgnss 60 Ermewa (2016) Fake",
+		["sgns_err_name_fake"] = "Sgnss 60 ERR (2006) Fake",
+		["sgns_gatx_name_fake"] = "Sgns 60 GATX (2019) Fake",
+		["sgns_gts_name_fake"] = "Sgnss 60 GTS (2011) Fake",
+		["sgns_hupac_name_fake"] = "Sgnss 60 Hupac (1981) Fake",
+		["sgns_hupac2_name_fake"] = "Sgnss 60 Hupac (2000) Fake",
+		["sgns_hupac3_name_fake"] = "Sgnss 60 Hupac (2023) Fake",
+		["sgns_kombiverkehr_name_fake"] = "Sgns 60 Kombiverkehr (1991) Fake",
+		["sgns_mfd_name_fake"] = "Sgns 60 MFD (2020) Fake",
+		["sgns_rca_name_fake"] = "Sgnss 60 RCA (2008) Fake",
+		["sgns_rca2_name_fake"] = "Sgnss 60 RCA (1990) Fake",
+		["sgns_sbb_name_fake"] = "Sgnss 60 SBB (1995) Fake",
+		["sgns_stb_name_fake"] = "Sgnss 60 StB (2005) Fake",
+		["sgns_vtg_name_fake"] = "Sgns 60 VTG (2000) Fake",
+
+		["sgssy_name_fake"] = "Sgss-y 703 Kombiwaggon (1991) Fake",
+		["sgssy2_name_fake"] = "Sgss 703 Kombiwaggon (1998) Fake",
+
+		--- Fake Ganzzug ---
+		["sgns_gts_name_gts_fake"] = "Sgnss 60 GTS (2011) GTS Fake",
+		["sgssy_name_pic_fake"] = "Sgss-y 703 Kombiwaggon (1991) PIC Fake",
+		["sgssy2_name_pic_fake"] = "Sgss 703 Kombiwaggon (1998) PIC Fake",
+	},
+}
+end
